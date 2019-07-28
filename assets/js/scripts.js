205cf40c9703cb81554ab8d7c2f6845ca165fad5
Carina <carrie.poell@gmail.com>
2019-07-28 16:08:18 +0200

    create and connect scripts.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+ 
+ 
+ $(document).ready(function () {
+      $("#nanoGallery3").nanoGallery({
+          itemsBaseURL:'http://brisbois.fr/nanogallery/demonstration/'
+      });
+  });
+        
+});
