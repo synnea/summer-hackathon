47311206f786d222a1a31ce310c29effc85a89dc
Carina <carrie.poell@gmail.com>
2019-07-29 02:08:57 +0200

    add iconsports section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 
     $(function () {
         $("#iconkidsmark").hide();
+        $("#iconsportsmark").hide();
     });
 
     // Upon clicking on the IconKids tag, it appears
@@ -14,7 +15,9 @@ $(document).ready(function () {
         $("#iconkidsbutton").on("click", function () {
             $(this).addClass("selected");
             $("#iconmanbutton").removeClass("selected");
+            $("iconsportsbutton").removeClass("selected");
             $("#iconmanmark").hide();
+            $("#iconsportsmark").hide();
             $("#iconkidsmark").show();
         });
     });
@@ -23,62 +26,23 @@ $(document).ready(function () {
         $("#iconmanbutton").on("click", function () {
             $(this).addClass("selected");
             $("#iconkidsbutton").removeClass("selected");
+            $("#iconsportsbutton").removeClass("selected");
             $("#iconkidsmark").hide();
+            $("#iconsportsmark").hide();
             $("#iconmanmark").show();
         });
     });
 
-    // // The "Home" button takes the user back to the landing page.
+    $(function () {
+        $("#iconsportsbutton").on("click", function () {
+            $(this).addClass("selected");
+            $("#iconkidsbutton").removeClass("selected");
+            $("#iconmanbutton").removeClass("selected");
+            $("#iconkidsmark").hide();
+            $("#iconmanmark").hide();
+            $("#iconsportsmark").show();
+        });
+    });
 
-    // $(function () {
-    //     $("#navHome").on("click", function () {
-    //         $(".dashboard-container").hide();
-    //         $(".main").fadeIn(500);
-    //         $(".content-container").removeClass("background-alt").addClass("background");
-    //     });
-    // });
-
-    // // The following prevents the graphs from breaking by clicking on bar labels.
-
-    // $(function () {
-    //     $(".barLabel").on("click", function () {
-    //         dc.redrawAll();
-    //     });
-    // });
-
-    // // ------------------------------------ LANDING PAGE ---------------------------------------
-
-    // // The CTA button takes the user to the first dashboard.
-
-    // $(function () {
-    //     $("#main-btn").on("click", function () {
-    //             $(".main").hide();
-    //             $(".dashboard-container").fadeIn(500);
-    //             $(".content-container").removeClass("background").addClass("background-alt");
-    //             // The following line starts intro.js upon click of the main button.
-    //             introJs().start();
-    //         });
-    //     });
-
-
-    // // ------------------------------------ SMOOTH SCROLLING ---------------------------------------
-
-    // // A smooth animation takes the user to the desired dashboard
-
-    // $('a[href*="#"]').on('click',function(e) {
-    //     e.preventDefault();
-    //     var target = this.hash;
-    //     var $target = $(target);
-    //     $('html, body').stop().animate({
-    //      'scrollTop': $target.offset().top
-    //     }, 900, 'swing', function () {
-    //      window.location.hash = target;
-    //     });
-    //    });
-
-    //    $('#resetTotalViews').click(function() {
-    //     dc.filterAll();
-    //     dc.renderAll();
-    // }); 
 
 });
